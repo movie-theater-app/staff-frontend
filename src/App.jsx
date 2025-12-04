9880f48d055364ae7273691e3a1e992d2f4e49f5
Anniina-55 <anniina.leikas55@gmail.com>
2025-12-04 04:53:08 +0200

    Implemented  ProtectedRoute component and added it to routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import StaffDashboard from "./pages/StaffDashboard";
 import AddTheatre from './pages/AddTheatre';
 import AddMovie from "./pages/AddMovie.jsx";
 import ScheduleMovie from "./pages/ScheduleMovie.jsx";
-//import SeatMapPage from "./pages/SeatMapPage.jsx";
 import EditMovie from "./pages/EditMovie.jsx"
 import Login from './pages/LoginView.jsx';
 import Logout from "./components/Logout.jsx";
+import ManageStaff from "./pages/manageStaff.jsx";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 
 function App() {
@@ -16,12 +17,51 @@ function App() {
     <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
-        <Route path="/dashboard" element={<StaffDashboard />} />
-        <Route path="/add-theatre" element={<AddTheatre />} />
-        <Route path="/add-movie" element={<AddMovie />} />
-        <Route path="/add-theatre/seat-map/:auditoriumId" element={<AddTheatre showOverlayRoute />} />
-        <Route path="/movie/:id/schedule" element={<ScheduleMovie />} />
-        <Route path="/edit-movie" element={<EditMovie />} />
+
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <StaffDashboard />
+          </ProtectedRoute>} 
+        />
+        <Route path="/add-theatre" element={
+          <ProtectedRoute>
+            <AddTheatre />
+          </ProtectedRoute>} 
+        />
+        <Route path="/add-movie" element={
+          <ProtectedRoute>
+            <AddMovie />
+          </ProtectedRoute>} 
+        />
+        <Route path="/add-theatre/seat-map/:auditoriumId" element={
+          <ProtectedRoute>
+            <AddTheatre showOverlayRoute />
+          </ProtectedRoute>} 
+        />
+        <Route path="/movie/:id/schedule" element={
+          <ProtectedRoute>
+            <ScheduleMovie />
+          </ProtectedRoute>} 
+        />
+        <Route path="/edit-movie" element={
+          <ProtectedRoute>
+            <EditMovie />
+          </ProtectedRoute>} 
+        />
+        <Route path="/manage-staff" element={
+          <ProtectedRoute requireAdmin={true}>
+            <ManageStaff/>
+          </ProtectedRoute>}
+        />
+        {/* <Route 
+        path="/statistics" 
+        element={
+          <ProtectedRoute requireAdmin={true}>
+            <Statistics />
+          </ProtectedRoute>
+        } 
+      /> */}
+
     </Routes>
   )
 }
